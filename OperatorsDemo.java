30e560f68126c53e39e4b5542da7a758ab783d6d
Bit-Basu <kc0260102@gmail.com>
2026-05-16 01:45:23 -0700

    Add files via upload

---
+++ OperatorsDemo.java
@@ -0,0 +1,28 @@
+public class OperatorsDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        // Arithmetic
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        // Logical
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y);
+        System.out.println(x || y);
+        System.out.println(!x);
+    }
+}
---
+++ VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 50000.50;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "Basu";
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    }
+}
